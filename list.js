48a5bcbb158400af44b46b56e77496cf580b1098
fsuffieldcode <fsuffieldcode@gmail.com>
2019-08-26 22:10:52 +0100

    add features and refactor

--- list.js
+++ list.js
@@ -1,46 +1,45 @@
-window.setTimeout(function() {
-  var todos = ["Buy New Turtle"];
+window.setTimeout(function () {
+
+
+
+  var todos = [];
 
   var input = prompt("What would you like to do?");
 
   while (input !== "quit") {
     //handle input
     if (input === "list") {
-      console.log(todos);
+      listTodos();
     } else if (input === "new") {
-      //ask for new todo
-      var newTodo = prompt("Enter new todo");
-      //add to todos array
-      todos.push(newTodo);
-      console.log(todos);
+      addTodo();
+    } else if (input === "delete") {
+      deleteTodo();
     }
     //ask again for new input
     input = prompt("What would you like to do?");
   }
   console.log("OK, YOU QUIT THE APP");
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  // put all of your JS code from the lecture here
-  }, 500);
 
+  function listTodos() {
+    console.log("***********");
+    todos.forEach(function (todo, i) {
+      console.log(i + ": " + todo);
+    });
+    console.log("***********");
+  }
 
+  function addTodo() {
+    //ask for new todo
+    var addTodo = prompt("Enter new todo");
+    //add to todos array
+    todos.push(addTodo);
+    console.log(addTodo + " added to list");
+  }
 
- 
+  function deleteTodo() {
+    //ask for index of todo to be deleted
+    var index = prompt("Enter  index of todo to delete");
+    //delete that todo
+    todos.splice(index, 1);
+  }
+}, 500);
